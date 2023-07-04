2f4e261550b41b2ff68bc5234884c6575b85fcd5
Anushka-2021 <annamilk2021@mail.ru>
2023-07-04 21:42:55 +0300

    Database added; it saves every request into db "usingdb.db", table "myStrings1"

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-# This is a sample Python script.
 import sqlite3, numpy as np, requests, cv2
 from flask import Flask, request, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
-conn = sqlite3.connect("usingdb.db")
+conn = sqlite3.connect("usingdb.db", check_same_thread=False)
 cursor = conn.cursor()
 
 @app.route('/', methods = ['POST', 'GET'])
 def sending():
     if request.method == 'POST':
-        str = request.form.get('entr')
+        text = request.form.get('entr')
         image = np.zeros((100, 100, 3), dtype=np.uint8)
-        # Устанавливаем параметры текста
-        text = str
+        # Text parameters
         font = cv2.FONT_HERSHEY_SIMPLEX
         font_scale = 1
-        font_color = (255, 255, 255)  # Белый цвет текста
+        font_color = (255, 255, 255)  # White
         line_type = cv2.LINE_AA
 
-        # Определяем параметры видео
-        fps = 30  # Количество кадров в секунду
-        video_length = 3  # Длина видео в секундах
+        # Video parameters
+        fps = 30
+        video_length = 3
         total_frames = fps * video_length
 
         # Video creation
@@ -29,19 +28,22 @@ def sending():
         video_writer = cv2.VideoWriter('running_text.mp4', fourcc, fps, (100, 100))
 
         text_width, text_height = cv2.getTextSize(text, font, font_scale, 1)[0]
-        print(total_frames, text_width)
         x = 100
-        y = (100 + text_height) // 2
+        y = 50
         for frame_number in range(total_frames):
-            # Очищаем изображение
             image.fill(0)
 
             # Drawing text into video and pushing it into existing video
             cv2.putText(image, text, (x, y), font, font_scale, font_color, 1, line_type)
             video_writer.write(image)
 
-            x-=(text_width+200)//total_frames
+            x-=(text_width+200)//total_frames # offset
         video_writer.release()
+
+        # Adding into database
+        f = list(cursor.execute("SELECT count(*) FROM myStrings1"))[0][0]
+        cursor.execute("INSERT OR REPLACE INTO myStrings1(string_id, string_body, time_of_sending) VALUES (?, ?, ?)", (f+1, text, datetime.now()))
+       # print(list(cursor.execute("SELECT * FROM myStrings1")))
     return render_template("test.html")
 
 if __name__ == '__main__':
@@ -50,6 +52,4 @@ if __name__ == '__main__':
     """)
     conn.commit()
 
-
     app.run(debug=True)
-    print("!")
